a517ca388556a379035d255b2a05664774ad6051
Kornel Kielczewski <kornel.kielczewski@allegrogroup.com>
2015-05-12 20:22:26 +0200

    Hash collisions

--- src/main/r/hash-distribution.R
+++ src/main/r/hash-distribution.R
@@ -1,15 +1,44 @@
 files <- c("/tmp/hashes-words-mm3.csv",
-           "/tmp/hashes-words-multi-mm3-0.csv",
-           "/tmp/hashes-words-multi-mm3-1.csv",
-           "/tmp/hashes-words-multi-mm3-5.csv",
-           "/tmp/hashes-words-multi-mm3-10.csv")
+           "/tmp/hashes-words-fnv.csv",
+           "/tmp/hashes-words-multi-fnv-0.csv",
+           "/tmp/hashes-words-multi-fnv-1.csv",
+           "/tmp/hashes-words-multi-fnv-5.csv",
+           "/tmp/hashes-words-multi-fnv-10.csv")
 
-csvs <- lapply(files, function(x) c(x, read.csv(x)))
+#files <- c("/tmp/hashes-words-mm3.csv")
 
-par(mfrow = c(3, 2)) 
-for (csv in csvs) {
-  data <- csv[0]
-  filename <- csv[1]
-  plot(density(csv$hash), main = filename)
+read_data <- function(filename) {
+  return(c(filename, read.csv(filename)))
 }
+
+get_distribution <- function(csv) {
+  filename <- csv[1]
+  min <- min(csv$hash) + 1
+  max <- max(csv$hash)
+  hash2 <- (csv$hash - min) / (max - min)
+  uhash2 <- unique(hash2)
+  return(c(filename,uhash2))
+}
+
+dists <- lapply(lapply(files, read_data), get_distribution)
+
+#par(mfrow = c(3, 2))
+for (csv in csvs) {
+  filename <- csv[1]
+  #plot(csv$hash, main = filename, xaxt = "l")
+  #plot(density(csv$hash), main = filename, xaxt = "l")  
+  min <- min(csv$hash) + 1
+  max <- max(csv$hash)
+  hash2 <- (csv$hash - min) / (max - min)
+  uhash2 <- unique(hash2)
+  return uhash2
+  #df <- data.frame(uhash2)
+  #m <- ggplot(df, aes(x=uhash2))
+  #m + geom_histogram(aes(fill = ..count.. ))
+  
+  #plot(density(uhash2), main = filename, xaxt = "l")
+  #pv <- ks.test(uhash2, "punif")$p.value
+  #print(c(filename, pv))
+}
+
 
--- src/main/scala/org/kornel/bloom/app/HashDistributionApp.scala
+++ src/main/scala/org/kornel/bloom/app/HashDistributionApp.scala
@@ -2,23 +2,31 @@ package org.kornel.bloom.app
 
 import java.io.FileWriter
 
-import org.kornel.bloom.hash.n32.{FNV, MultiHash}
+import org.kornel.bloom.hash.n32.{MurMur3, FNV, MultiHash}
 import org.kornel.bloom.hash.Hash
 
 object HashDistributionApp extends App {
 
-  val max = 10000
-
   val allWords = AllWords()
 
+  //val max = math.min(10000, allWords.length)
+  val max = allWords.length
+
+  println(s"Working on $max elements...")
+
   val base = FNV
-  probe(base, "mm3")
-  probe(MultiHash(base, 0), "multi-mm3-0")
-  probe(MultiHash(base, 1), "multi-mm3-1")
-  probe(MultiHash(base, 5), "multi-mm3-5")
-  probe(MultiHash(base, 10), "multi-mm3-10")
+
+  probe(MurMur3, "mm3")
+  probe(base, "fnv")
+  probe(MultiHash(base, 0), "multi-fnv-0")
+  probe(MultiHash(base, 1), "multi-fnv-1")
+  probe(MultiHash(base, 5), "multi-fnv-5")
+  probe(MultiHash(base, 10), "multi-fnv-10")
 
   private def probe[N](hashFunc: Hash[N], name: String): Unit = {
+
+    //val randomNumber = scala.util.Random.shuffle((0 until allWords.length).toList).toIterator
+
     val fileName = s"/tmp/hashes-words-$name.csv"
 
     println(s"$fileName")
@@ -26,13 +34,22 @@ object HashDistributionApp extends App {
     val tw = new FileWriter(fileName)
     tw.write("hash,word\n")
 
+    val hashes = scala.collection.mutable.Set[N]()
+
     (0 until max).foreach {
       n =>
-        val idx = (math.random * allWords.length).toInt
+        val idx = n //randomNumber.next
         val word = allWords(idx)
         val hash = hashFunc(word)
+
+        hashes += hash
+
         tw.write(s"$hash,$word\n")
     }
+
+    val collisions = max - hashes.size
+
+    println(s"Collisions: $collisions")
 
     tw.close()
   }
